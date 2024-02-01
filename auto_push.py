66b5a110ff369d4242589a5f5385ea0d105b57e8
Rahimi <Zaman.Rahimi@energytransfer.com>
2024-02-01 14:55:37 -0600

    this is a test commit1

--- auto_push.py
+++ auto_push.py
@@ -1,6 +1,6 @@
 import subprocess as sb 
+
+commit_msg = "this is a test commit1" 
 sb.run("git add .", shell=True)
-sb.run("git commit -m 'test'", shell=True)
+sb.run('git commit -m "{}"'.format(commit_msg), shell=True)
 sb.run("git push", shell=True)
-
-
